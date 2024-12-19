6fd6467be5e1e529324e51a115c15d2f6fa7a6b8
CrawKatt <man849916@gmail.com>
2024-12-19 18:57:14 -0300

    Add Basic Trunk Placer

--- src/main/java/com/delu/ancienttimes/AncientTimes.java
+++ src/main/java/com/delu/ancienttimes/AncientTimes.java
@@ -1,5 +1,6 @@
 package com.delu.ancienttimes;
 
+import com.delu.ancienttimes.common.worldgen.tree.ModTrunkPlacerTypes;
 import com.delu.ancienttimes.registries.*;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -50,6 +51,7 @@ public class AncientTimes {
         ModBlocks.BLOCKS.register(modEventBus);
         ModLootModifiers.LOOT_MODIFIER_SERIALIZER.register(modEventBus);
         ModEntities.ENTITIES.register(modEventBus);
+        ModTrunkPlacerTypes.register(modEventBus);
 
         ModCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
 
