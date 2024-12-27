ee7245434ad788d8d3f0a0ae099cd8090e2fbd31
CrawKatt <man849916@gmail.com>
2024-12-26 23:38:22 -0300

    Remove Basic Trunk Placer and Basic Foliage Placer

--- src/main/java/com/delu/ancienttimes/AncientTimes.java
+++ src/main/java/com/delu/ancienttimes/AncientTimes.java
@@ -1,24 +1,14 @@
 package com.delu.ancienttimes;
 
-import com.delu.ancienttimes.common.worldgen.tree.ModFoliagePlacerTypes;
-import com.delu.ancienttimes.common.worldgen.tree.ModTrunkPlacerTypes;
 import com.delu.ancienttimes.registries.*;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -52,8 +42,6 @@ public class AncientTimes {
         ModBlocks.BLOCKS.register(modEventBus);
         ModLootModifiers.LOOT_MODIFIER_SERIALIZER.register(modEventBus);
         ModEntities.ENTITIES.register(modEventBus);
-        ModTrunkPlacerTypes.register(modEventBus);
-        ModFoliagePlacerTypes.register(modEventBus);
 
         ModCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         ModPlacedFeatures.register(modEventBus);
