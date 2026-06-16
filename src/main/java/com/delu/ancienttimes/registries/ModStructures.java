fa581c06f68ad2e7fec0b81e7400c2495ecc71b4
Bretzelfresser <bretzelfresser@gmail.com>
2026-06-16 17:10:34 +0200

    jungle ruin spawns now

--- src/main/java/com/delu/ancienttimes/registries/ModStructures.java
+++ src/main/java/com/delu/ancienttimes/registries/ModStructures.java
@@ -1,20 +1,17 @@
 package com.delu.ancienttimes.registries;
 
 import com.delu.ancienttimes.AncientTimes;
-import com.delu.ancienttimes.common.structure.JungleRuinStructure;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.heightproviders.ConstantHeight;
-import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
-import net.minecraft.world.level.levelgen.heightproviders.TrapezoidHeight;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
 
 import java.util.Map;
 import java.util.Objects;
@@ -26,7 +23,7 @@ public class ModStructures {
 
 
     public static void generate(BootstapContext<Structure> ctx){
-        ctx.register(JUNGLE_RUIN_STRUCTURE, new JungleRuinStructure(
+        ctx.register(JUNGLE_RUIN_STRUCTURE, new JigsawStructure(
                 new Structure.StructureSettings(
                         ctx.lookup(Registries.BIOME).getOrThrow(ModTags.Biomes.JUNGLE_RUIN_CAN_SPAWN),
                         Map.of(),
@@ -34,8 +31,10 @@ public class ModStructures {
                         TerrainAdjustment.BEARD_THIN
                 ),
                 ctx.lookup(Registries.TEMPLATE_POOL).getOrThrow(ModStructurePools.JUNGLE_RUINS_POOL),
-                200,
-                TrapezoidHeight.of(VerticalAnchor.aboveBottom(30), VerticalAnchor.belowTop(30))
+                7,
+                ConstantHeight.of(VerticalAnchor.absolute(0)),
+                false,
+                Heightmap.Types.WORLD_SURFACE_WG
         ));
         
     }
